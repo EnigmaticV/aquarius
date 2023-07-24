3bd114d738645bb5196eed6729b28dc75695e822
Vipul Pal <91409639+EnigmaticV@users.noreply.github.com>
2023-07-24 19:53:08 +0530

    Update Aquarius AccountMenu.tsx

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -15,16 +15,16 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
   }
 
   return (
-    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+      <div className="bg-gradient-to-br from-gray-300 to-zinc-300 bg-opacity-50 w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-3">
         <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
           <img className="w-8 rounded-md" src="/images/default-blue.png" alt="" />
-          <p className="text-white text-sm group-hover/item:underline">{currentUser?.name}</p>
+          <p className="text-black text-sm group-hover/item:underline">hi, {currentUser?.name}!</p>
         </div>
       </div>
       <hr className="bg-gray-600 border-0 h-px my-4" />
-      <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
-        Sign out of Netflix
+      <div onClick={() => signOut()} className="px-3 text-center text-black text-sm hover:underline">
+        Sign out
       </div>
     </div>
   )
