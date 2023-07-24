0dfac0e587d60afa388d3be907c43e1607eeb4af
Vipul Pal <91409639+EnigmaticV@users.noreply.github.com>
2023-07-24 19:30:54 +0530

    aquarius auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -68,17 +68,18 @@ const Auth = () => {
   }, [email, name, password, login]);
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-5">
-          <img src="/images/logo.png" className="h-12" alt="Logo" />
-        </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
+ <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-gradient-to-br from-slate-50 to-zinc-300 w-full h-full lg:bg-opacity-50">
+      <div className="h-screen flex items-center justify-center">
+      <nav className="px-12 py-5"> 
+      <img src="/images/logoBlack.png" alt="logo" className="h-12" />
+                </nav>
+                <div className="bg-gradient-to-br from-gray-300 to-zinc-300 bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-sm text-black mb-8 opacity-70 uppercase ">
+            
               {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 text-sm ">
               {variant === 'register' && (
                 <Input
                   id="name"
@@ -103,28 +104,27 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)} 
               />
             </div>
-            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={variant === 'login' ? login : register} className="text-sm bg-zinc-800 py-3 text-white rounded-md w-full mt-10 hover:bg-zinc-900 transition uppercase">
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-              <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+              <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white bg-opacity-40 rounded-full flex items-center justify-center cursor-pointer hover:bg-opacity-80 transition">
                 <FcGoogle size={32} />
               </div>
-              <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+              <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white bg-opacity-40 rounded-full flex items-center justify-center cursor-pointer hover:bg-opacity-80 transition">
                 <FaGithub size={32} />
               </div>
             </div>
-            <p className="text-neutral-500 mt-12">
+            <p className="text-center text-sm text-neutral-500 mt-12">
               {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                {variant === 'login' ? 'Create an account' : 'Login'}
+              <span onClick={toggleVariant} className="text-gray-50 ml-1 hover:underline cursor-pointer font-bold">
+                {variant === 'login' ? 'Create an Account' : 'Login'}
               </span>
-              .
             </p>
           </div>
         </div>
       </div>
-    </div>
+   </div>
   );
 }
 
