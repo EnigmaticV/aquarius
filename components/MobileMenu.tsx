058b8be57537f566d3bcb2b36e1efa9520a0945a
Vipul Pal <91409639+EnigmaticV@users.noreply.github.com>
2024-01-19 15:47:05 +0530

    Update MobileMenu.tsx |  2 - 19th January 2023

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -13,22 +13,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-4">
         <div className="px-3 text-center text-white hover:underline">
-          Home
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Series
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Films
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          New & Popular
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          My List
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Browse by Languages
+          <a href="#library">Library</a>
         </div>
         <div className="px-3 text-center text-white hover:underline">
           <a href="#watchlist">Watchlist</a>
