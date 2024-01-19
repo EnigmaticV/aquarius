6a8939c58bc1bacfd5be6193a96b5df5d4f79575
Vipul Pal <91409639+EnigmaticV@users.noreply.github.com>
2024-01-19 17:42:13 +0530

    Update auth.tsx 17 - 19th January 2024

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -68,10 +68,9 @@ const Auth = () => {
   }, [email, name, password, login]);
 
   return (
-    <head>
-      <title>Aquarius</title>
-      </head>
+    
  <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+   <head><title>Aquarius</title></head>
       <div className="bg-gradient-to-br from-slate-50 to-zinc-300 w-full h-full lg:bg-opacity-50 px-6 lg:px-0 md:px-10">
       <div className="h-screen flex items-center justify-center">
       <nav className="px-12 py-5 hidden sm:block hidden md:hidden lg:block"> 
